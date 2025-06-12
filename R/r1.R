7f65b375239eaa0becba5a99b271bf519246357d
Jaskirat <jaskiratmann99@gmail.com>
2025-06-12 12:12:44 -0700

    My R exercise

--- R/r1.R
+++ R/r1.R
@@ -6,6 +6,21 @@ f1 <- function(n) {
   squares_sum
 }
 
-n <- 1:10000
+###test function 2
 
-f1(n)
+f2<-function(n){
+  squares_sum <- 0 
+  for(i in 1:length(n)) {
+    squares_sum <- squares_sum + n[i]^2
+  }
+  squares_sum
+}
+##test function 3
+f3<-function (n){
+sum(n^2)
+}
+
+#bench marking
+library(bench)
+n<-100
+print(mark(f1(n),f2(n),f3(n)))
