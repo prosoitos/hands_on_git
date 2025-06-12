17d2825788b4db14112942f78ff26fe3526ca25c
Jaskirat <jaskiratmann99@gmail.com>
2025-06-12 12:14:35 -0700

    R exercise

--- R/r1.R
+++ R/r1.R
@@ -8,19 +8,11 @@ f1 <- function(n) {
 
 ###test function 2
 
-f2<-function(n){
-  squares_sum <- 0 
-  for(i in 1:length(n)) {
-    squares_sum <- squares_sum + n[i]^2
-  }
-  squares_sum
-}
-##test function 3
-f3<-function (n){
-sum(n^2)
+f2<-function (n){
+  sum(n^2)
 }
 
 #bench marking
 library(bench)
 n<-100
-print(mark(f1(n),f2(n),f3(n)))
+print(mark(f1(n),f2(n)))
