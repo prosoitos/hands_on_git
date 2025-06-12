b07985f5b4a9f699231bad2d8b3ed28bb3ed5890
Hrishabh Srivastava <hrishabhsrivastava3@gmail.com>
2025-06-12 12:08:52 -0700

    Modified bash2.sh

--- bash/bash2.sh
+++ bash/bash2.sh
@@ -1,2 +1,12 @@
-function countfiles() {
-}
+function countlines(){
+if [ $# == 0 ];
+then
+ echo "No arguments given. Usage: countfiles dir1 dir2 ..."
+else
+ for i in $@
+ do
+  echo $i has $(cd $i | ls -l | wc -l) files
+ done
+fi
+ }
+
