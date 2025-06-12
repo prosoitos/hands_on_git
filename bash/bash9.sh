7c487e55d0253ec014eda327e9d2778a2ab89ade
Lars-B <berlinglars96@gmail.com>
2025-06-12 12:06:22 -0700

    bash9.sh update

--- bash/bash9.sh
+++ bash/bash9.sh
@@ -1,1 +1,10 @@
-# write a loop to replace spaces to underscores in all file names in the current directory
+#!/bin/bash
+   
+for file in *; do
+       if [[ -f "$file" ]]; then
+           new_file="${file// /!}"
+	   if [[ $file != $new_file ]]; then
+           	mv "$file" "$new_file"
+	   fi
+       fi
+done
