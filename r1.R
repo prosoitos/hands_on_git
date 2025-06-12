ef1662ff577add41d72d53b56b09410d5d05bccd
Angell0901 <qujunhuaangell@gmail.com>
2025-06-12 12:09:47 -0700

    add improvement for R script 1

---
+++ r1.R
@@ -0,0 +1,17 @@
+f1 <- function(n) {
+  squares_sum <- 0
+  for(i in 1:length(n)) {
+    squares_sum <- squares_sum + n[i]^2
+  }
+  squares_sum
+}
+
+n <- 1:10000
+
+f1(n)
+
+f2 <- function(n) {
+	sum(n^2)
+}
+
+mark(f1(n), f2(n))
